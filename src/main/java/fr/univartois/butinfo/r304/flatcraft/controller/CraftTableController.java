9ca98ede08953e1ec9a15d08ce7b7c1359c68f93
Louis Dutombois <louis.dutombois@gmail.com>
2023-12-01 14:52:58 +0100

    Commit des changement pour les mob

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/controller/CraftTableController.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/controller/CraftTableController.java
@@ -20,6 +20,7 @@ import java.util.Optional;
 
 import fr.univartois.butinfo.r304.flatcraft.model.FlatcraftGame;
 import fr.univartois.butinfo.r304.flatcraft.model.resources.Resource;
+import fr.univartois.butinfo.r304.flatcraft.view.SpriteStore;
 import javafx.fxml.FXML;
 import javafx.geometry.HPos;
 import javafx.geometry.VPos;
@@ -105,8 +106,8 @@ public final class CraftTableController {
                 resourceViews[i][j] = new ImageView();
 
                 // On configure la taille appropriée pour la vue.
-                resourceViews[i][j].setFitHeight(16);
-                resourceViews[i][j].setFitWidth(16);
+                resourceViews[i][j].setFitHeight(SpriteStore.getInstance().getSpriteSize());
+                resourceViews[i][j].setFitWidth(SpriteStore.getInstance().getSpriteSize());
 
                 // On positionne la vue correctement.
                 GridPane.setHalignment(resourceViews[i][j], HPos.CENTER);
